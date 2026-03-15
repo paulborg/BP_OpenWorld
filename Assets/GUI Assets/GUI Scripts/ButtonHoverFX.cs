f10382c8b89dea818fb6524d45157aa4f3e2325f
paulborg <apurplegoat.temp@gmail.com>
2026-03-15 04:33:41 +0100

    GUI Update, Packages, Assets, Terrain ++
    PACKAGES
    - Added LeanTween Package for UI animations
    
    GUI UPDATES
    - Additional "OnHover" effects for Main Menu UI, using LeanTween
    - New Custom Journal Background Image Asset
    - Responsive journal entry / quest log list UI & custom quest icon sprite for testing (newspaper quest.)
    Data/Functionality not connected yet, but already set up and future-proofed for dynamically changing entries with quest info/descriptions.
    - Added & set up custom slider sprites for options/settings menus.
    
    ASSETS
    - Added a few more "completed" assets & textures, with the final(ish?) artstyle
        - Large Window (For commercial "Main Street" buildings.)
        - Double Doors (Also for commercial "Main Street" buildings.)
        - Metal Fence + Fence Post (previously used in Old-Town alleyway mockup)
        - Stone Corner Trim (usable both in "Main St." and "Old Town."
    
    ++ OTHER
    - Continued mockups/blockouts of "Main Street" in personal testing scene.
    - Renamed copied one of the Scenes/Levels to "...Beta_01" of clarity.
    This is our new shared "main" scene going forwards.
    - Added terrain to the new main "Beta" scene and created a first rough sculpt of the ground around old town, beach, park pond and lighthouse surroundings.

--- Assets/GUI Assets/GUI Scripts/ButtonHoverFX.cs
+++ Assets/GUI Assets/GUI Scripts/ButtonHoverFX.cs
@@ -6,8 +6,7 @@ public class ButtonHoverFX : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 {
     [Header("Circle Animation")]
     public Animator circleAnimator;
-
-    private bool isHovering;
+    //private bool isHovering = false;
 
 
     //++ Scale Effect (Pick and import tweening libraries for better/easier controls)
@@ -15,9 +14,15 @@ public class ButtonHoverFX : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     //++ Sound Trigger
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Awake()
+    {
+
+    }
+
     void Start()
     {
-        
+        //isHovering = false;
     }
 
     // Update is called once per frame
@@ -26,20 +31,42 @@ public class ButtonHoverFX : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         
     }
 
+    void OnEnable()
+    {
+        //isHovering = false;
+
+        ResetHoverState();
+    }
+
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        isHovering = true;
+        //isHovering = true;
 
         if (circleAnimator != null && !circleAnimator.GetCurrentAnimatorStateInfo(0).IsName("Draw Selection"))
         {
             circleAnimator.SetBool("isHovering", true);
             circleAnimator.Play("Draw Selection", 0, 0);
         }
+        gameObject.LeanScale(new Vector3(2, 2), 0.2f).setEaseInOutBack();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        isHovering = false;
+        //isHovering = false;
         circleAnimator.SetBool("isHovering", false);
+
+        gameObject.LeanScale(new Vector3(1.5f, 1.5f), 0.2f).setEaseInOutBack();
     }
+
+    public void ResetHoverState()
+    {
+        Debug.Log("ResetHover Called");
+        //isHovering = false;
+        circleAnimator.SetBool("isHovering", false);
+        circleAnimator.Rebind();
+        circleAnimator.Update(0f);
+        gameObject.LeanScale(new Vector3(1.5f, 1.5f), 0f);
+    }
+
 }
