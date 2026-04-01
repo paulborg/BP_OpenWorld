4600c4d2193bfd1945ba096ce543e8ba46889ba8
paulborg <apurplegoat.temp@gmail.com>
2026-04-01 18:10:04 +0200

    Character pack setup, NPC pathing script update
    - Exploring new character pack & animations, and figuring out setup.
    
    - Updated NPC pathing script, adding smoother character rotation when switching to next checkpoint.

--- Assets/Scripts/NPC_Pathing.cs
+++ Assets/Scripts/NPC_Pathing.cs
@@ -5,26 +5,51 @@ public class NPC_Pathing : MonoBehaviour
 {
     public Transform[] checkPoints;
     public int targetPoint;
-    public float speed;
-    private Transform lookTarget;
+    public float walkSpeed = 1f;
+    public float rotationSpeed = 5f;
+    //private Transform lookTarget;  -- From original NPC pathing.
 
     void Start()
     {
-        if (lookTarget = null)
-        {
-            lookTarget = checkPoints[targetPoint].transform;
-        }
+        // -- From original NPC pathing, unused in updated version. -- //
+
+        //if (lookTarget = null)
+        //{
+        //    lookTarget = checkPoints[targetPoint].transform;
+        //}
     }
 
     
     void Update()
     {
-        if (Vector3.Distance(transform.position, checkPoints[targetPoint].position) <= 0.02f)
+        // -- ORIGINAL NPC_PATHING -- //
+
+        //if (Vector3.Distance(transform.position, checkPoints[targetPoint].position) <= 0.02f)
+        //{
+        //    IncreaseTargetInt();
+        //}
+        //transform.position = Vector3.MoveTowards(transform.position, checkPoints[targetPoint].position, walkSpeed * Time.deltaTime);
+        //transform.LookAt(checkPoints[targetPoint].transform);
+
+
+        // -- UPDATE NPC_PATHING, SMOOTHER ROTATIONS -- //
+
+        Vector3 targetDirection = checkPoints[targetPoint].position - transform.position;
+
+        if (targetDirection.magnitude <= 0.02f)
         {
             IncreaseTargetInt();
+            return;
         }
-        transform.position = Vector3.MoveTowards(transform.position, checkPoints[targetPoint].position, speed * Time.deltaTime);
-        transform.LookAt(checkPoints[targetPoint].transform);
+
+        transform.position = Vector3.MoveTowards(transform.position, checkPoints[targetPoint].position, walkSpeed * Time.deltaTime);
+
+        if (targetDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        
     }
     
     void IncreaseTargetInt()
